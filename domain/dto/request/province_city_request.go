3939c20fa58c0902d5425520ffba85bd8a64382c
Fauzan <fauzan.c4@gmail.com>
2025-03-18 08:04:12 +0700

    create province_city_request.go

---
+++ domain/dto/request/province_city_request.go
@@ -0,0 +1,9 @@
+package request
+
+type GetByProvinceIDRequest struct {
+	ProvinceID	int `params:"prov_id" validate:"required"`
+}
+
+type GetByCityIDRequest struct {
+	CityID int `params:"city_id" validate:"required"`
+}
