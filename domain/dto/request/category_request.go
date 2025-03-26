f253c99efed438e4f4a50d051c60a7c805dc16a3
Fauzan <fauzan.c4@gmail.com>
2025-03-27 06:34:04 +0700

    struct alignment category request

--- domain/dto/request/category_request.go
+++ domain/dto/request/category_request.go
@@ -1,9 +1,12 @@
 package request
 
+import "github.com/fauzan264/evermos-rakamin/domain/model"
+
 type GetByCategoryIDRequest struct {
-	ID					int 		`params:"id" validate:"required"`
+	ID					int 			`params:"id" validate:"required"`
 }
 
 type CategoryRequest struct {
-	NamaCategory		string		`json:"nama_category" validate:"required"`
+	NamaCategory		string			`json:"nama_category" validate:"required"`
+	User				model.User
 }
