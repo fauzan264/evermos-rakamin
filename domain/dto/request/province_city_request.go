98252979ec82db5f96a43948c091a89fb75847cc
Fauzan <fauzan.c4@gmail.com>
2025-03-27 06:34:35 +0700

    struct alignment province city request

--- domain/dto/request/province_city_request.go
+++ domain/dto/request/province_city_request.go
@@ -1,9 +1,9 @@
 package request
 
 type GetByProvinceIDRequest struct {
-	ProvinceID	int `params:"prov_id" validate:"required"`
+	ProvinceID		int 	`params:"prov_id" validate:"required"`
 }
 
 type GetByCityIDRequest struct {
-	CityID int `params:"city_id" validate:"required"`
+	CityID 			int 	`params:"city_id" validate:"required"`
 }
