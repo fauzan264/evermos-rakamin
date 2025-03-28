1dc4fb2e5ca7735a6bdcd25d4924a1a0a46b28f2
Fauzan <fauzan.c4@gmail.com>
2025-03-29 06:16:15 +0700

    create response formatter category & list category

--- domain/dto/response/category_response.go
+++ domain/dto/response/category_response.go
@@ -1,6 +1,10 @@
 package response
 
-import "time"
+import (
+	"time"
+
+	"github.com/fauzan264/evermos-rakamin/domain/model"
+)
 
 type CategoryResponse struct {
 	ID				int			`json:"id"`
@@ -8,3 +12,23 @@ type CategoryResponse struct {
 	CreatedAt		*time.Time	`json:"created_at,omitempty"`
 	UpdatedAt		*time.Time	`json:"updated_at,omitempty"`
 }
+
+func CategoryResponseFormatter(category model.Category) CategoryResponse {
+	categoryResponse := CategoryResponse{
+		ID: category.ID,
+		NamaCategory: category.NamaCategory,
+	}
+
+	return categoryResponse
+}
+
+func ListCategoryResponseFormatter(listCategory []model.Category) []CategoryResponse {
+	var listCategoryResponse []CategoryResponse
+	for _, category := range listCategory {
+		categoryResponseFormatter := CategoryResponseFormatter(category)
+		listCategoryResponse = append(listCategoryResponse, categoryResponseFormatter)
+	}
+
+	return listCategoryResponse
+}
+
