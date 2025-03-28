b60665ddecc5fca9d8494605abe16e5c3400ce0a
Fauzan <fauzan.c4@gmail.com>
2025-03-29 06:17:16 +0700

    add softdelete for category model

--- domain/model/category_model.go
+++ domain/model/category_model.go
@@ -1,12 +1,17 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Category struct {
 	ID 				int				`gorm:"type:int;primaryKey;autoIncrement"`
 	NamaCategory 	string			`gorm:"type:varchar(255);not null"`
 	CreatedAt 		time.Time		`gorm:"type:timestamp;not null;default:current_timestamp"`
 	UpdatedAt 		time.Time		`gorm:"type:timestamp"`
+	DeletedAt     	gorm.DeletedAt 	`gorm:"index"`
 }
 
 func (Category) TableName() string {
