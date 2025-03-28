86266620ba3fa1ca153ffbb35189364564dd250c
Fauzan <fauzan.c4@gmail.com>
2025-03-29 04:59:20 +0700

    create shop response formatter

--- domain/dto/response/shop_response.go
+++ domain/dto/response/shop_response.go
@@ -1,7 +1,30 @@
 package response
 
+import "github.com/fauzan264/evermos-rakamin/domain/model"
+
 type TokoResponse struct {
 	ID       	int 		`json:"id"`
 	NamaToko 	string 		`json:"nama_toko"`
 	URLFoto  	string 		`json:"url_foto"`
+}
+
+
+func ShopResponseFormatter(shop model.Toko) TokoResponse {
+	shopResponse := TokoResponse{
+		ID: shop.ID,
+		NamaToko: shop.NamaToko,
+		URLFoto: shop.URLFoto,
+	}
+
+	return shopResponse
+}
+
+func ListShopResponseFormatter(listShop []model.Toko) []TokoResponse {
+	var listShopResponse []TokoResponse
+	for _, trx := range listShop {
+		shopResponseFormatter := ShopResponseFormatter(trx)
+		listShopResponse = append(listShopResponse, shopResponseFormatter)
+	}
+
+	return listShopResponse
 }
