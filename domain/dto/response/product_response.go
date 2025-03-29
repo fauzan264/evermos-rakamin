8184723bacd8871488844a6fdb38026760bf5ceb
Fauzan <fauzan.c4@gmail.com>
2025-03-29 10:16:07 +0700

    create detail product & list product response formatter

--- domain/dto/response/product_response.go
+++ domain/dto/response/product_response.go
@@ -1,5 +1,7 @@
 package response
 
+import "github.com/fauzan264/evermos-rakamin/domain/model"
+
 type ProductResponse struct {
 	ID				int						`json:"id"`
 	NamaProduk		string					`json:"nama_produk"`
@@ -29,4 +31,46 @@ type LogProductResponse struct {
 	Toko			ShopResponse			`json:"toko"`
 	Category		CategoryResponse		`json:"category"`
 	Photos			[]PhotoProductResponse	`json:"photos"`
+}
+
+func ProductResponseFormatter(product model.Product) ProductResponse {
+	productShop := ShopResponseFormatter(product.Toko)
+	
+	productCategory := CategoryResponseFormatter(product.Category)
+	
+	var productPhotos []PhotoProductResponse
+	for _, photo := range product.PhotosProduct {
+		photoProduct := PhotoProductResponse{
+			ID: photo.ID,
+			IDProduk: photo.IDProduk,
+			URL: photo.URL,
+		}
+
+		productPhotos = append(productPhotos, photoProduct)
+	}
+
+	productResponse := ProductResponse{
+		ID: product.ID,
+		NamaProduk: product.NamaProduk,
+		Slug: product.Slug,
+		HargaReseller: product.HargaReseller,
+		HargaKonsumen: product.HargaKonsumen,
+		Stok: product.Stok,
+		Deskripsi: product.Deskripsi,
+		Toko: productShop,
+		Category: productCategory,
+		Photos: productPhotos,
+	}
+
+	return productResponse
+}
+
+func ListProductResponseFormatter(listProduct []model.Product) []ProductResponse {
+	var listProductResponse []ProductResponse
+	for _, product := range listProduct {
+		productResponseFormatter := ProductResponseFormatter(product)
+		listProductResponse = append(listProductResponse, productResponseFormatter)
+	}
+
+	return listProductResponse
 }
