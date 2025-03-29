1719c290821aa6202ba9fe4c1e5e838cae9aac84
Fauzan <fauzan.c4@gmail.com>
2025-03-29 08:35:09 +0700

    create load config

--- config/config.go
+++ config/config.go
@@ -2,9 +2,24 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
+
+type Config struct {
+	SecretKey 	string
+	AppHost		string
+	AppPort		string
+}
+
+func LoadConfig() *Config {
+	return &Config{
+		SecretKey: os.Getenv("SECRET_KEY"),
+		AppHost: os.Getenv("APP_HOST"),
+		AppPort: os.Getenv("APP_PORT"),
+	}
+}
 
 func init() {
 	if err := godotenv.Load(); err != nil {
