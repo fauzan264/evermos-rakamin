dd157887a2b47610a407d5acfb779431382d0e96
Fauzan <fauzan.c4@gmail.com>
2025-03-29 08:32:41 +0700

    create helper get image url

---
+++ helpers/url_helper.go
@@ -0,0 +1,26 @@
+package helpers
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fauzan264/evermos-rakamin/config"
+)
+
+func GetImageURL(path string) string {
+	cfg := config.LoadConfig()
+
+	if !strings.HasPrefix(cfg.AppHost, "http://") && !strings.HasPrefix(cfg.AppHost, "https://") {
+		cfg.AppHost = "http://" + cfg.AppHost
+	}
+
+	baseURL := fmt.Sprintf("%s:%s", cfg.AppHost, cfg.AppPort)
+
+	// Jika path sudah full URL, langsung return
+	if strings.HasPrefix(path, "http://") || strings.HasPrefix(path, "https://") {
+		return path
+	}
+
+	fmt.Println(fmt.Sprintf("%s/%s", baseURL, path))
+	return fmt.Sprintf("%s/%s", baseURL, path)
+}
